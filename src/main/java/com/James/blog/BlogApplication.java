dcb2a3b8dece9ac9a54f759a27585ea9f3e7e7b7
张佳鹏 <zhangjiapeng@webull.com>
2024-09-08 21:28:46 +0800

    inti project

--- src/main/java/com/James/blog/BlogApplication.java
+++ src/main/java/com/James/blog/BlogApplication.java
@@ -2,10 +2,11 @@ package com.James.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class BlogApplication {
 
